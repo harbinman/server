82577d5d6dcfcd31ab149089fae965f484c943f7
harbinman <106586639+harbinman@users.noreply.github.com>
2023-09-17 10:09:05 +0800

    new index

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import routeV1 from "./routes/index.js";
 
 const app = express();
 
+app.keepAliveTimeout = 65000;
+app.headersTimeout = 66000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,10 +22,17 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-  server.listen(port, () => console.log(`Server is listening on port ${port}`));
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected");
+    server.listen(port, () =>
+      console.log(`Server is listening on port ${port}`)
+    );
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
+app.keepAliveTimeout = 65000;
+app.headersTimeout = 66000;
